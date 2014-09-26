46eea0e5eb8a9e4ad58106855865efb07af9ca37
Stefan Grundmann <sg2342@googlemail.com>
2014-09-26 04:23:54 +0000

    add null functions for the unsupported pam module types

--- pam_ocra/pam_ocra.c
+++ pam_ocra/pam_ocra.c
@@ -122,6 +122,48 @@ pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char *argv[])
 	return PAM_SUCCESS;
 }
 
-#ifdef PAM_MODULE_ENTRY
+PAM_EXTERN int
+pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char *argv[])
+{
+	(void)pamh;
+	(void)flags;
+	(void)argc;
+	(void)argv;
+
+	return PAM_SUCCESS;
+}
+
+PAM_EXTERN int
+pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char *argv[])
+{
+	(void)pamh;
+	(void)flags;
+	(void)argc;
+	(void)argv;
+
+	return PAM_SUCCESS;
+}
+
+PAM_EXTERN int
+pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char *argv[])
+{
+	(void)pamh;
+	(void)flags;
+	(void)argc;
+	(void)argv;
+
+	return PAM_SUCCESS;
+}
+
+PAM_EXTERN int
+pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char *argv[])
+{
+	(void)pamh;
+	(void)flags;
+	(void)argc;
+	(void)argv;
+
+	return PAM_SUCCESS;
+}
+
 PAM_MODULE_ENTRY("pam_ocra");
-#endif
